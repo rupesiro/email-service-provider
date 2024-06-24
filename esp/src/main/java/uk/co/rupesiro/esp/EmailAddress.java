fdaab31a17c2be565fbc49655b1a539458932a75
Rui Rosário <code@rupesiro.co.uk>
2024-06-24 06:45:26 +0100

    Add Javadoc to EmailAddress

--- esp/src/main/java/uk/co/rupesiro/esp/EmailAddress.java
+++ esp/src/main/java/uk/co/rupesiro/esp/EmailAddress.java
@@ -6,6 +6,9 @@ import lombok.Value;
 
 import java.util.regex.Pattern;
 
+/**
+ * Representation of an email address according to the guidelines provided in the OWASP website.
+ */
 @Value
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 class EmailAddress {
@@ -14,7 +17,13 @@ class EmailAddress {
             "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$"
     );
 
+    /**
+     * The local part (i.e. the portion before the {code @}) of an email address.
+     */
     String localPart;
+    /**
+     * The host (i.e. the portion after the {code @}) of an email address.
+     */
     String host;
 
     @Override
@@ -22,6 +31,15 @@ class EmailAddress {
         return localPart + '@' + host;
     }
 
+    /**
+     * Constructs an email address instance from the raw email address supplied, validating it according to the OWASP
+     * guidelines.
+     *
+     * @param emailAddress the raw email address.
+     * @return the constructed email address.
+     *
+     * @see <a href="https://owasp.org/www-community/OWASP_Validation_Regex_Repository">OWASP Guidelines</a>
+     */
     static EmailAddress from(String emailAddress) {
         if (emailAddress == null || !EMAIL_ADDRESS_PATTERN.matcher(emailAddress).find())
             throw new IllegalArgumentException("Invalid email address: " + emailAddress);
